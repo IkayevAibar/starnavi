89ac321c47d654271ca7393aff7f008d39921a32
Aybar <aibarchik2019@gmail.com>
2022-06-21 14:37:40 +0600

    init and models added

---
+++ app/models.py
@@ -0,0 +1,30 @@
+from cgitb import text
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+# Create your models here.
+
+
+# class User_Information(models.Model):
+#     user = models.ForeignKey(User, verbose_name="User", on_delete=models.CASCADE)
+#     avatar = 
+
+class Post(models.Model):
+    '''
+        *is_reply* is needed to make sure that post is not reply or comment to another post
+    '''
+    text = models.TextField(max_length=5000, blank=True, null=True)
+    created_at = models.DateTimeField("Post created date", default=timezone.now)
+    published = models.BooleanField("Published",default=False)
+    is_reply = models.BooleanField("Reply for post",default=False)
+    author = models.ForeignKey(User, verbose_name="Author of post", on_delete=models.CASCADE)
+    parent_post = models.ForeignKey("self", verbose_name="Branch parent post", on_delete=models.SET_NULL, null=True, blank=True)
+
+# class Post_File(models.Model):
+#     post = models.ForeignKey(Post, verbose_name="Post", on_delete=models.CASCADE)
+
+class Like(models.Model):
+    post = models.ForeignKey(Post, verbose_name="Post", on_delete=models.CASCADE)
+    user = models.ForeignKey(User, verbose_name="User", on_delete=models.CASCADE)
+    datetime = models.DateTimeField("Post created date", default=timezone.now)
+
